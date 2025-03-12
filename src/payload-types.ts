35c5a00774837c1df7994a01d67bd30bfedff13a
James <james@trbl.design>
2025-03-12 12:14:02 -0400

    chore: init

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -6,23 +6,116 @@
  * and re-run `payload generate:types` to regenerate this file.
  */
 
+/**
+ * Supported timezones in IANA format.
+ *
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "supportedTimezones".
+ */
+export type SupportedTimezones =
+  | 'Pacific/Midway'
+  | 'Pacific/Niue'
+  | 'Pacific/Honolulu'
+  | 'Pacific/Rarotonga'
+  | 'America/Anchorage'
+  | 'Pacific/Gambier'
+  | 'America/Los_Angeles'
+  | 'America/Tijuana'
+  | 'America/Denver'
+  | 'America/Phoenix'
+  | 'America/Chicago'
+  | 'America/Guatemala'
+  | 'America/New_York'
+  | 'America/Bogota'
+  | 'America/Caracas'
+  | 'America/Santiago'
+  | 'America/Buenos_Aires'
+  | 'America/Sao_Paulo'
+  | 'Atlantic/South_Georgia'
+  | 'Atlantic/Azores'
+  | 'Atlantic/Cape_Verde'
+  | 'Europe/London'
+  | 'Europe/Berlin'
+  | 'Africa/Lagos'
+  | 'Europe/Athens'
+  | 'Africa/Cairo'
+  | 'Europe/Moscow'
+  | 'Asia/Riyadh'
+  | 'Asia/Dubai'
+  | 'Asia/Baku'
+  | 'Asia/Karachi'
+  | 'Asia/Tashkent'
+  | 'Asia/Calcutta'
+  | 'Asia/Dhaka'
+  | 'Asia/Almaty'
+  | 'Asia/Jakarta'
+  | 'Asia/Bangkok'
+  | 'Asia/Shanghai'
+  | 'Asia/Singapore'
+  | 'Asia/Tokyo'
+  | 'Asia/Seoul'
+  | 'Australia/Sydney'
+  | 'Pacific/Guam'
+  | 'Pacific/Noumea'
+  | 'Pacific/Auckland'
+  | 'Pacific/Fiji';
+
 export interface Config {
   auth: {
     users: UserAuthOperations;
   };
+  blocks: {};
   collections: {
     users: User;
     media: Media;
+    posts: Post;
+    'posts-2': Posts2;
+    'posts-3': Posts3;
+    'posts-4': Posts4;
+    'posts-5': Posts5;
+    'posts-6': Posts6;
+    'posts-7': Posts7;
+    'posts-8': Posts8;
+    'posts-9': Posts9;
+    'posts-10': Posts10;
+    'posts-11': Posts11;
+    'posts-12': Posts12;
+    'payload-locked-documents': PayloadLockedDocument;
     'payload-preferences': PayloadPreference;
     'payload-migrations': PayloadMigration;
   };
+  collectionsJoins: {};
+  collectionsSelect: {
+    users: UsersSelect<false> | UsersSelect<true>;
+    media: MediaSelect<false> | MediaSelect<true>;
+    posts: PostsSelect<false> | PostsSelect<true>;
+    'posts-2': Posts2Select<false> | Posts2Select<true>;
+    'posts-3': Posts3Select<false> | Posts3Select<true>;
+    'posts-4': Posts4Select<false> | Posts4Select<true>;
+    'posts-5': Posts5Select<false> | Posts5Select<true>;
+    'posts-6': Posts6Select<false> | Posts6Select<true>;
+    'posts-7': Posts7Select<false> | Posts7Select<true>;
+    'posts-8': Posts8Select<false> | Posts8Select<true>;
+    'posts-9': Posts9Select<false> | Posts9Select<true>;
+    'posts-10': Posts10Select<false> | Posts10Select<true>;
+    'posts-11': Posts11Select<false> | Posts11Select<true>;
+    'posts-12': Posts12Select<false> | Posts12Select<true>;
+    'payload-locked-documents': PayloadLockedDocumentsSelect<false> | PayloadLockedDocumentsSelect<true>;
+    'payload-preferences': PayloadPreferencesSelect<false> | PayloadPreferencesSelect<true>;
+    'payload-migrations': PayloadMigrationsSelect<false> | PayloadMigrationsSelect<true>;
+  };
   db: {
-    defaultIDType: string;
+    defaultIDType: number;
   };
   globals: {};
+  globalsSelect: {};
   locale: null;
   user: User & {
     collection: 'users';
+  };
+  jobs: {
+    tasks: unknown;
+    workflows: unknown;
   };
 }
 export interface UserAuthOperations {
@@ -48,7 +141,7 @@ export interface UserAuthOperations {
  * via the `definition` "users".
  */
 export interface User {
-  id: string;
+  id: number;
   updatedAt: string;
   createdAt: string;
   email: string;
@@ -65,7 +158,7 @@ export interface User {
  * via the `definition` "media".
  */
 export interface Media {
-  id: string;
+  id: number;
   alt: string;
   updatedAt: string;
   createdAt: string;
@@ -81,13 +174,240 @@ export interface Media {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts".
+ */
+export interface Post {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-2".
+ */
+export interface Posts2 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-3".
+ */
+export interface Posts3 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-4".
+ */
+export interface Posts4 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-5".
+ */
+export interface Posts5 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-6".
+ */
+export interface Posts6 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-7".
+ */
+export interface Posts7 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-8".
+ */
+export interface Posts8 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-9".
+ */
+export interface Posts9 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-10".
+ */
+export interface Posts10 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-11".
+ */
+export interface Posts11 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-12".
+ */
+export interface Posts12 {
+  id: number;
+  title?: string | null;
+  slug?: string | null;
+  writtenBy?: (number | null) | User;
+  updatedAt: string;
+  createdAt: string;
+  _status?: ('draft' | 'published') | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "payload-locked-documents".
+ */
+export interface PayloadLockedDocument {
+  id: number;
+  document?:
+    | ({
+        relationTo: 'users';
+        value: number | User;
+      } | null)
+    | ({
+        relationTo: 'media';
+        value: number | Media;
+      } | null)
+    | ({
+        relationTo: 'posts';
+        value: number | Post;
+      } | null)
+    | ({
+        relationTo: 'posts-2';
+        value: number | Posts2;
+      } | null)
+    | ({
+        relationTo: 'posts-3';
+        value: number | Posts3;
+      } | null)
+    | ({
+        relationTo: 'posts-4';
+        value: number | Posts4;
+      } | null)
+    | ({
+        relationTo: 'posts-5';
+        value: number | Posts5;
+      } | null)
+    | ({
+        relationTo: 'posts-6';
+        value: number | Posts6;
+      } | null)
+    | ({
+        relationTo: 'posts-7';
+        value: number | Posts7;
+      } | null)
+    | ({
+        relationTo: 'posts-8';
+        value: number | Posts8;
+      } | null)
+    | ({
+        relationTo: 'posts-9';
+        value: number | Posts9;
+      } | null)
+    | ({
+        relationTo: 'posts-10';
+        value: number | Posts10;
+      } | null)
+    | ({
+        relationTo: 'posts-11';
+        value: number | Posts11;
+      } | null)
+    | ({
+        relationTo: 'posts-12';
+        value: number | Posts12;
+      } | null);
+  globalSlug?: string | null;
+  user: {
+    relationTo: 'users';
+    value: number | User;
+  };
+  updatedAt: string;
+  createdAt: string;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "payload-preferences".
  */
 export interface PayloadPreference {
-  id: string;
+  id: number;
   user: {
     relationTo: 'users';
-    value: string | User;
+    value: number | User;
   };
   key?: string | null;
   value?:
@@ -107,11 +427,220 @@ export interface PayloadPreference {
  * via the `definition` "payload-migrations".
  */
 export interface PayloadMigration {
-  id: string;
+  id: number;
   name?: string | null;
   batch?: number | null;
   updatedAt: string;
   createdAt: string;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "users_select".
+ */
+export interface UsersSelect<T extends boolean = true> {
+  updatedAt?: T;
+  createdAt?: T;
+  email?: T;
+  resetPasswordToken?: T;
+  resetPasswordExpiration?: T;
+  salt?: T;
+  hash?: T;
+  loginAttempts?: T;
+  lockUntil?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "media_select".
+ */
+export interface MediaSelect<T extends boolean = true> {
+  alt?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  url?: T;
+  thumbnailURL?: T;
+  filename?: T;
+  mimeType?: T;
+  filesize?: T;
+  width?: T;
+  height?: T;
+  focalX?: T;
+  focalY?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts_select".
+ */
+export interface PostsSelect<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-2_select".
+ */
+export interface Posts2Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-3_select".
+ */
+export interface Posts3Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-4_select".
+ */
+export interface Posts4Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-5_select".
+ */
+export interface Posts5Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-6_select".
+ */
+export interface Posts6Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-7_select".
+ */
+export interface Posts7Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-8_select".
+ */
+export interface Posts8Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-9_select".
+ */
+export interface Posts9Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-10_select".
+ */
+export interface Posts10Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-11_select".
+ */
+export interface Posts11Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "posts-12_select".
+ */
+export interface Posts12Select<T extends boolean = true> {
+  title?: T;
+  slug?: T;
+  writtenBy?: T;
+  updatedAt?: T;
+  createdAt?: T;
+  _status?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "payload-locked-documents_select".
+ */
+export interface PayloadLockedDocumentsSelect<T extends boolean = true> {
+  document?: T;
+  globalSlug?: T;
+  user?: T;
+  updatedAt?: T;
+  createdAt?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "payload-preferences_select".
+ */
+export interface PayloadPreferencesSelect<T extends boolean = true> {
+  user?: T;
+  key?: T;
+  value?: T;
+  updatedAt?: T;
+  createdAt?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "payload-migrations_select".
+ */
+export interface PayloadMigrationsSelect<T extends boolean = true> {
+  name?: T;
+  batch?: T;
+  updatedAt?: T;
+  createdAt?: T;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,15 +3,37 @@ import { sqliteAdapter } from '@payloadcms/db-sqlite'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
-import { buildConfig } from 'payload'
+import { buildConfig, CollectionConfig, PayloadRequest } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { opsCounterPlugin } from './plugins/opsCounter'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
+
+const createSampleCollection = (number?: number): CollectionConfig => ({
+  slug: `posts${number ? `-${number}` : ''}`,
+  versions: { drafts: true },
+  fields: [
+    {
+      name: 'title',
+      label: 'Title',
+      type: 'text',
+    },
+    {
+      name: 'slug',
+      type: 'text',
+    },
+    {
+      name: 'writtenBy',
+      type: 'relationship',
+      relationTo: 'users',
+    },
+  ],
+})
 
 export default buildConfig({
   admin: {
@@ -20,7 +42,142 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [
+    Users,
+    Media,
+    createSampleCollection(),
+    createSampleCollection(2),
+    createSampleCollection(3),
+    createSampleCollection(4),
+    createSampleCollection(5),
+    createSampleCollection(6),
+    createSampleCollection(7),
+    createSampleCollection(8),
+    createSampleCollection(9),
+    createSampleCollection(10),
+    createSampleCollection(11),
+    createSampleCollection(12),
+  ],
+  endpoints: [
+    {
+      path: '/test-graphql',
+      method: 'get',
+      handler: async (req: PayloadRequest) => {
+        const { payload } = req
+        const email = 'dev@payloadcms.com'
+        const password = 'test'
+
+        const existingUserQuery = await payload.find({
+          collection: 'users',
+          where: {
+            email: {
+              equals: email,
+            },
+          },
+        })
+
+        if (!existingUserQuery.docs[0]) {
+          await payload.create({
+            collection: 'users',
+            data: {
+              email,
+              password,
+            },
+          })
+        }
+
+        const { token } = await payload.login({
+          collection: 'users',
+          data: {
+            email,
+            password,
+          },
+        })
+
+        const variables = {
+          draft: true,
+          slug: 'hello',
+        }
+
+        const query = `query pageTypeBySlug($slug: String!, $draft: Boolean = false) {
+        Posts(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts2s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts3s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts4s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts5s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts6s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts7s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts8s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts9s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts10s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts11s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+        Posts12s(where: { slug: { equals: $slug } }, draft: $draft) {
+          docs {
+            id
+          }
+        }
+      }`
+
+        const json = await fetch('http://127.0.0.1:3000/api/graphql', {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            query,
+            variables,
+          }),
+        }).then((res) => res.json())
+
+        return Response.json(json)
+      },
+    },
+  ],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
@@ -34,6 +191,10 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
+    opsCounterPlugin({
+      max: 15,
+      warnAt: 12,
+    }),
     // storage-adapter-placeholder
   ],
 })
